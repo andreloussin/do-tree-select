259f3e3104d82b821199ca9b7a084d9c9c0b4d5b
LOUSSIN André <105634694+andreloussin@users.noreply.github.com>
2024-04-19 19:17:20 +0100

    Display TreeSelect

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" class="hidden" />
+  <HelloWorld msg="Welcome to Your Vue.js App" class="hidden" />
+  <TreeSelect />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TreeSelect from "./components/TreeSelect/TreeSelect.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    TreeSelect,
+  },
+};
 </script>
 
 <style>
